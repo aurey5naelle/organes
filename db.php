cc6de355b87527d5ae3e2e1ece645a761aa44bc1
aurey5naelle <144020304+aurey5naelle@users.noreply.github.com>
2026-07-31 15:08:30 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,10 +1,10 @@
 <?php
 // On récupère les valeurs depuis n'importe quelle superglobale disponible
-$host     = $_ENV['MYSQLHOST']     ?? $_SERVER['MYSQLHOST']     ?? getenv('MYSQLHOST')     ?: null;
-$port     = $_ENV['MYSQLPORT']     ?? $_SERVER['MYSQLPORT']     ?? getenv('MYSQLPORT')     ?: '3306';
-$dbname   = $_ENV['MYSQLDATABASE'] ?? $_SERVER['MYSQLDATABASE'] ?? getenv('MYSQLDATABASE') ?: null;
-$user     = $_ENV['MYSQLUSER']     ?? $_SERVER['MYSQLUSER']     ?? getenv('MYSQLUSER')     ?: null;
-$password = $_ENV['MYSQLPASSWORD'] ?? $_SERVER['MYSQLPASSWORD'] ?? getenv('MYSQLPASSWORD') ?: null;
+$host     = $_ENV['mysql.railway.internal']     ?? $_SERVER['mysql.railway.internal']     ?? getenv('mysql.railway.internal')     ?: null;
+$port     = $_ENV['3306']     ?? $_SERVER['3306']     ?? getenv('3306')     ?: '3306';
+$dbname   = $_ENV['railway'] ?? $_SERVER['railway'] ?? getenv('railway') ?: null;
+$user     = $_ENV['root']     ?? $_SERVER['root']     ?? getenv('root')     ?: null;
+$password = $_ENV['sABgKYFMXbouPClzlIBQlDrxLZogXxEL'] ?? $_SERVER['sABgKYFMXbouPClzlIBQlDrxLZogXxEL'] ?? getenv('sABgKYFMXbouPClzlIBQlDrxLZogXxEL') ?: null;
 
 // Si Railway n'a pas transmis MYSQLHOST, on affiche un message d'aide clair au lieu de planter sur localhost
 if (!$host) {
